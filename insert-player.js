ec602c284ed8e3f5c0a4daafa81d43cd533bcc02
AllohaTV <65871630+AllohaTV@users.noreply.github.com>
2020-11-17 16:08:55 +0200

    Update insert-player.js

--- insert-player.js
+++ insert-player.js
@@ -4,9 +4,11 @@ $(function(){
 		$.get('https://api.alloha.tv/', param_cdn, function(data){
 			data = $.parseJSON(data);
 			if ( data.status == 'success' ) {
+				console.log(data.status);
 				if ( 'calback_success' in param_cdn && typeof window[param_cdn.calback_error] == 'function' ) {
 					window[param_cdn.calback_success](data);
 				} else {
+					console.log('iframe');
 					$( param_cdn.player_id ).html( '<iframe frameborder="0" src="' + data.data.iframe + '" allowfullscreen></iframe>' );
 				}
 			} else {
