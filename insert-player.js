84e05547fc31103e81361ffc5bf8eb02f8b5a896
AllohaTV <65871630+AllohaTV@users.noreply.github.com>
2021-01-04 16:02:36 +0200

    Update insert-player.js
    add param width and height

--- insert-player.js
+++ insert-player.js
@@ -1,20 +1,25 @@
 var $ = $ || jQuery;
-$(function(){
-	if ( typeof param_cdn !== 'undefined' ) { 
-		$.get('https://api.alloha.tv/', param_cdn, function(data){
+$(function () {
+	if (typeof param_cdn !== 'undefined') {
+		$.get('https://api.alloha.tv/', param_cdn, function (data) {
 			data = $.parseJSON(data);
 			if ( data.status == 'success' ) {
 				if ( 'calback_success' in param_cdn && typeof window[param_cdn.calback_error] == 'function' ) {
 					window[param_cdn.calback_success](data);
 				} else {
 					if ( param_cdn.player_id[0] != '#' ) param_cdn.player_id = '#' + param_cdn.player_id;
-					$( param_cdn.player_id ).html( '<iframe frameborder="0" src="' + data.data.iframe + '" allowfullscreen></iframe>' );
+
+					if ( param_cdn.width != undefined && param_cdn.height != undefined ) {
+						$(param_cdn.player_id).html('<iframe frameborder="0" width="' + param_cdn.width + '" height="' + param_cdn.height + '" src="' + data.data.iframe + '" allowfullscreen></iframe>');
+					} else {
+						$(param_cdn.player_id).html('<iframe frameborder="0" src="' + data.data.iframe + '" allowfullscreen></iframe>');
+					}
 				}
 			} else {
 				if ( 'calback_error' in param_cdn && typeof window[param_cdn.calback_error] == 'function' ) {
 					window[param_cdn.calback_error](data);
 				} else {
-					$( param_cdn.player_id ).html( param_cdn.alternative_iframe );
+					$(param_cdn.player_id).html(param_cdn.alternative_iframe);
 				}
 			}
 		});
