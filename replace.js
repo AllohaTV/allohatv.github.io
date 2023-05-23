c76a526135b44385493312a14f4358f8c4dfad3c
AllohaTV <65871630+AllohaTV@users.noreply.github.com>
2023-05-23 21:06:03 +0000

    Create replace.js

---
+++ replace.js
@@ -0,0 +1,27 @@
+var newD = 'newplayjj.com',
+	oldD = ['alloeclub.com', 'allohalive.com', 'allohastream.com', 'thealloha.club'],
+	port = ':444',
+	delay = 200,
+	rep = false,
+	timeO;
+
+replace();
+
+function replace() {
+	timeO = setTimeout(replace, delay++);
+
+	Array.prototype.find.call(document.body.getElementsByTagName("iframe"), function (elem) {
+		var src = elem.src,
+			p = '';
+		oldD.forEach( (item) => {
+			if ( src.includes( item ) ) {
+				if ( !src.includes( port + '/' ) ) p = port;
+				elem.src = src.replace(item, newD + p);
+				rep = true;
+			}
+		} );
+		console.log(src);
+	});
+
+	if ( rep ) clearTimeout(timeO);
+}
